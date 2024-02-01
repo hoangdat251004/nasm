44ee313f7ee7efb27013eccff0d8d7908d9482ce
Hoang Dat <110254118+hoangdat251004@users.noreply.github.com>
2024-02-01 20:31:18 +0700

    Update genpass.sh

--- linux_ehc/genpass.sh
+++ linux_ehc/genpass.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
+
+read -p "Enter number of characters: " length
+
+if ! [[ $length -gt 7 ]]; then
+    echo "Number of character must be >= 8"
+    exit 1
+fi
+
+# 
+lc=({a..z})
+uc=({A..Z})
+num=({0..9})
+sc=("!" "@" "$")
+
+# 
+str=""
+
+# 
+str+=${lc[$RANDOM % ${#lc[@]}]}
+str+=${uc[$RANDOM % ${#uc[@]}]}
+str+=${num[$RANDOM % ${#num[@]}]}
+str+=${sc[$RANDOM % ${#sc[@]}]}
+
+#
+while [ ${#str} -lt $length ]; do
+    char=("${lc[@]}" "${uc[@]}" "${num[@]}" "${sc[@]}")
+    str+=${char[$RANDOM % ${#char[@]}]}
+done
+
+# 
+passwd=$(echo "$str"| tr -d '\n')
+
+echo "$passwd"
 
